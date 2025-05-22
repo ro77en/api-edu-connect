beb46d6415ae2a4ae2eabd4e41d8cb0cbac3a30c
ro77en <lucas.mdantas@icloud.com>
2025-05-22 19:56:10 -0300

    initial commit

---
+++ src/test/java/br/com/ro77en/edu_connect/EduConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ro77en.edu_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
